74f786004cb4d8c2de88c6040bdc37066019fc5c
lekdup <lekdup@live.fr>
2023-05-29 14:55:33 +0200

    all in app component

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,14 +1,63 @@
-import CalcForm from '../CalcForm/CalcForm';
-import Keypad from '../Keypad/Keypad';
-import Theme from '../Theme/Theme';
 import './App.scss';
+
+export const ACTIONS = {
+  ADD_DIGIT:  "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+} 
+function reducer(state, { type, payload }) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state
+      }
+    default:
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <Theme />
-      <CalcForm />
-      <Keypad />
+      <header className="Theme">
+        <h1>calc</h1>
+        <div className="Theme-toggleBtn">
+          <p>THEME</p>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+      </header>
+      <form className="CalcForm">
+        <input
+          type="number"
+          inputMode="decimal"
+          className="CalcForm-value"
+        />
+      </form>
+      <section className="Keypad">
+        <button type="button" value="7" className="num" >7</button>
+        <button type="button" value="8">8</button>
+        <button type="button" value="9">9</button>
+        <button id="del" type="button">DEL</button>
+        <button type="button" value="4">4</button>
+        <button type="button" value="5">5</button>
+        <button type="button" value="6">6</button>
+        <button type="button">+</button>
+        <button type="button" value="1">1</button>
+        <button type="button" value="2">2</button>
+        <button type="button" value="3">3</button>
+        <button type="button">-</button>
+        <button type="button">.</button>
+        <button type="button" value="0">0</button>
+        <button type="button">/</button>
+        <button type="button">x</button>
+        <button className="Keypad-reset" id="reset" type="button">RESET</button>
+        <button className="Keypad-equal" id="equal" type="button">=</button>
+      </section>
     </div>
   );
 }
