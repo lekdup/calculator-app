3ebc5a689ffd77ae6594e38c49a23779cc121834
lekdup <lekdup@live.fr>
2023-05-29 21:46:33 +0200

    hooks done with useReducer

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,3 +1,6 @@
+import { useReducer } from 'react';
+import DigitButton from './DigitButton';
+import OperationButton from './OperationButton';
 import './App.scss';
 
 export const ACTIONS = {
@@ -10,14 +13,87 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if(payload.digit === "0" && state.currentOperand === "0") return state
+      if(payload.digit === "." && state.currentOperand.includes(".")) return state
+
       return {
-        ...state
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`
+      };
+    
+    case ACTIONS.CLEAR:
+      return {};
+    
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand === null && state.previousOperand === null) return state;
+      if(state.currentOperand == null){
+        return {
+          ...state,
+          operation: payload.operation,
+        }
+      }
+
+      if(state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        }
+      }
+      
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
+      }
+    
+    case ACTIONS.DELETE_DIGIT:
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
+      };
+
+    case ACTIONS.EVALUATE:
+      return {
+        ...state,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state)
       }
     default:
   }
+  console.log(state.previousOperand);
+} 
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return "";
+
+  let calculate = ""
+  switch (operation) {
+    case "+":
+      calculate = prev + current
+      break
+    case "-":
+      calculate = prev - current
+      break
+    case "x":
+      calculate = prev * current
+      break
+    case "/":
+      calculate = prev / current
+      break
+  default:
+  }
+
+  return calculate.toString();
 }
 
 function App() {
+  const [{ currentOperand }, dispatch] = useReducer(reducer, {})
   return (
     <div className="App">
       <header className="Theme">
@@ -31,32 +107,46 @@ function App() {
           </ul>
         </div>
       </header>
-      <form className="CalcForm">
-        <input
-          type="number"
-          inputMode="decimal"
-          className="CalcForm-value"
-        />
-      </form>
+      <div className="CalcForm">
+        <div className="CalcForm-value">{currentOperand}</div>
+      </div>
       <section className="Keypad">
-        <button type="button" value="7" className="num" >7</button>
-        <button type="button" value="8">8</button>
-        <button type="button" value="9">9</button>
-        <button id="del" type="button">DEL</button>
-        <button type="button" value="4">4</button>
-        <button type="button" value="5">5</button>
-        <button type="button" value="6">6</button>
-        <button type="button">+</button>
-        <button type="button" value="1">1</button>
-        <button type="button" value="2">2</button>
-        <button type="button" value="3">3</button>
-        <button type="button">-</button>
-        <button type="button">.</button>
-        <button type="button" value="0">0</button>
-        <button type="button">/</button>
-        <button type="button">x</button>
-        <button className="Keypad-reset" id="reset" type="button">RESET</button>
-        <button className="Keypad-equal" id="equal" type="button">=</button>
+        <DigitButton digit="7" dispatch={dispatch}/>
+        <DigitButton digit="8" dispatch={dispatch}/>
+        <DigitButton digit="9" dispatch={dispatch}/>
+        <button
+          id="del"
+          onClick={() => {
+            dispatch({ type: ACTIONS.DELETE_DIGIT })
+          }}
+        >DEL
+        </button>
+        <DigitButton digit="4" dispatch={dispatch}/>
+        <DigitButton digit="5" dispatch={dispatch}/>
+        <DigitButton digit="6" dispatch={dispatch}/>
+        <OperationButton operation="+" dispatch={dispatch} />
+        <DigitButton digit="1" dispatch={dispatch}/>
+        <DigitButton digit="2" dispatch={dispatch}/>
+        <DigitButton digit="3" dispatch={dispatch}/>
+        <OperationButton operation="-" dispatch={dispatch} />
+        <DigitButton digit="." dispatch={dispatch}/>
+        <DigitButton digit="0" dispatch={dispatch}/>
+        <OperationButton operation="/" dispatch={dispatch} />
+        <OperationButton operation="x" dispatch={dispatch} />
+        <button
+          id="reset"
+          onClick={() => {
+            dispatch({ type: ACTIONS.CLEAR })
+          }}
+        >RESET
+        </button>
+        <button
+          id="equal"
+          onClick={() => {
+            dispatch({ type: ACTIONS.EVALUATE })
+          }}
+        >=
+        </button>
       </section>
     </div>
   );
